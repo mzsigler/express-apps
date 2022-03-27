9320235b7eababa985ccff861ac35a858bb174e5
mzsigler <mzsigler@gmail.com>
2022-03-27 15:12:19 -0500

    lots of small stylistic changes. Removed the Something Else button until I think of a use for it

--- app.js
+++ app.js
@@ -29,47 +29,47 @@ function calculateTax() {
         
     } else {
         let stateTax = salePrice * .07;
-        p.innerText+= `State Tax is $${stateTax.toFixed(2)}
+        p.innerText+= `State Tax: $${stateTax.toFixed(2)}
     `
 
     if (salePrice > 1599) {
         var localTax = 44.00;
-        p.innerText+= `Local Tax is $${localTax.toFixed(2)}
+        p.innerText+= `Local Tax: $${localTax.toFixed(2)}
         `    };
     
     if (salePrice <= 1599) {
         var localTax = salePrice * .0275;
-        p.innerText+= `Local Tax is $${localTax.toFixed(2)}
+        p.innerText+= `Local Tax: $${localTax.toFixed(2)}
         `
     };
 
     if (salePrice <= 1600) {
         var singleArticle = 0.00;
-        p.innerText+= `Single Article Tax is $${singleArticle.toFixed(2)}
+        p.innerText+= `Single Article Tax: $${singleArticle.toFixed(2)}
         `
     }
     if (salePrice > 3200) {
         var singleArticle = 44.00;
-        p.innerText+= `Single Article Tax is $${singleArticle.toFixed(2)}
+        p.innerText+= `Single Article Tax: $${singleArticle.toFixed(2)}
         `
     }
 
     if (salePrice >= 1600.01 && salePrice <=3200) {
         var singleArticle = (salePrice - 1600) * 0.0275;
-        p.innerText+= `Single Article Tax is $${singleArticle.toFixed(2)}
+        p.innerText+= `Single Article Tax: $${singleArticle.toFixed(2)}
         `
     }
 
     let totalLocalTax = localTax + singleArticle;
-    p.innerText+= `Total Local Tax is $${totalLocalTax.toFixed(2)}
+    p.innerText+= `Total Local Tax: $${totalLocalTax.toFixed(2)}
     `
 
     let totalTax = totalLocalTax + stateTax;
-    p.innerText+= `Total Tax is $${totalTax.toFixed(2)}
+    p.innerText+= `Total Tax: $${totalTax.toFixed(2)}
     `
 
     let totalPrice = totalTax + salePrice;
-    p.innerText+= `Total Price is $${totalPrice.toFixed(2)}
+    p.innerText+= `Total Price: $${totalPrice.toFixed(2)}
     `
 
     results.appendChild(p);
