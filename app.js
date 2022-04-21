f24311b8dbc0dfe76751c2a8f8cc345e392cf7c7
Mikel Sigler <mzsigler@gmail.com>
2022-04-20 20:47:10 -0500

    Added a table view for balance due results.
    Now I have a place to view results for a balance due search. Will probably make it the landing page for when you add a new balance due.

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+// Testing the balance due results page here:
+
+    let name = "Mikel Sigler";
+    let DLnumber = 090806158;
+    let contractDate = "05/28/2021";
+    let repo = "No";
+    let damages = "No";
+    let balance = 1245;
+    let notes = `Mr Sigler rented for three days local and drove the car to zimbabwe to buy crack
+    cocaine. In all fairness he did return the car in good condition, but was over his miles.`
+
+    let nameField = document.getElementById("name");
+    nameField.innerText = `${name}`;
+
+    let dlField = document.getElementById("dlNumber");
+    dlField.innerText = `${DLnumber}`;
+
+    let contractDateField = document.getElementById("contractDate");
+    contractDateField.innerText = `${contractDate}`;
+
+    let repoField = document.getElementById("repo");
+    repoField.innerText = `${repo}`;
+
+    let damagesField = document.getElementById("damages");
+    damagesField.innerText = `${damages}`;
+
+    let balanceField = document.getElementById("balance");
+    balanceField.innerText = `$${balance}`;
+
+    let notesField = document.getElementById("notes");
+    notesField.innerText = `${notes}`;
+
+// That will probably all be moved when I actually wire it up but it helps to have something there to style. 
+
 const taxCalcNav = document.getElementById("taxBtn");
 const greetingDiv = document.getElementById('greeting');
 const taxCalcDiv = document.getElementById("taxCalc");
